fdf5437fa102471e4f75c706f3dbf336fa5180f9
Nicolas Rusz <nrusz@digitalriver.com>
2018-02-28 14:08:12 -0300

    Spring discovery service

---
+++ src/main/java/com/edyne/microservices/springdiscoveryservice/SpringDiscoveryServiceApplication.java
@@ -0,0 +1,12 @@
+package com.edyne.microservices.springdiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDiscoveryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDiscoveryServiceApplication.class, args);
+	}
+}
